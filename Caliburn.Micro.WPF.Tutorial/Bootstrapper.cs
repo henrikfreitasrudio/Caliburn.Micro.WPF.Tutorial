1e70a53eea9ade1b78a551daac49496a81009619
Henri <Henri@DESKTOP-UD0KQBV>
2023-04-30 21:39:05 -0300

    Add project files.

---
+++ Caliburn.Micro.WPF.Tutorial/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro.WPF.Tutorial.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro.WPF.Tutorial
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+
+            LogManager.GetLog = type => new DebugLog(type);
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await DisplayRootViewForAsync(typeof(ShellViewModel));
+        }       
+    }
+}
